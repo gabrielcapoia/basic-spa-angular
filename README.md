ddbc3cf38698d8bcb80f5b00a9d5a147765f1af6
Gabriel Capoia <gcapoia@gmail.com>
2022-03-31 10:56:05 -0300

    complete project

---
+++ README.md
@@ -0,0 +1,9 @@
+Project develop in the course: https://desenvolvedor.io/curso/desenvolvimento-spa-com-angular
+
+## Fake back-end
+Need to install https://www.npmjs.com/package/json-server for fake back-end
+Run `json-server --watch produtos.json`
+
+##Front-end
+Run `ng install`
+Run `ng run start`
---
+++ src/app/produtos/produto.ts
@@ -0,0 +1,17 @@
+// export class Produto {
+//     id: string;
+//     nome: string;
+//     valor: string;
+//     promocao: boolean;
+//     valorPromo: string;
+//     imagem: string;    
+// }
+
+export class Produto {
+    id: string = "";
+    nome: string = "";
+    valor: string = "";
+    promocao: boolean = false;
+    valorPromo: string = "";
+    imagem: string = "";    
+}
